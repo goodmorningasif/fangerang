ce78ef3198859f300aed93041573b6921784458f
goodmorningasif <aa@hellofriends.org>
2020-10-27 21:49:29 -0700

    add procfile

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const webpack = require('webpack');
 const webpackDevServer = require('webpack-dev-server');
 
-const config = require('./webpack.config.js');
+const config = require('./webpack.common.js');
 const options = {
   contentBase: '/.dist',
   hot: true,
@@ -11,6 +11,6 @@ webpackDevServer.addDevServerEntrypoints(config, options);
 const compiler = webpack(config);
 const server = new webpackDevServer(compiler, options);
 
-server.listen(5000, function() {
+server.listen(process.env.PORT || 5000, function() {
   console.log('Fangarang listening on port 5000!\n');
 });
