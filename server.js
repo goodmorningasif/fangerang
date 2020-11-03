7cd010459952a4ebf669f9d486cf9fceb1684021
goodmorningasif <aa@hellofriends.org>
2020-11-02 22:49:46 -0800

    remove host key

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const config = require('./webpack.common.js');
 const options = {
   contentBase: '/.dist',
   hot: true,
-  host: 'localhost',
+  // host: 'localhost',
 }
 webpackDevServer.addDevServerEntrypoints(config, options);
 const compiler = webpack(config);
 const server = new webpackDevServer(compiler, options);
 const port = process.env.PORT || 5000;
 
-server.listen(port, function() {
-  console.log(`Fangarang listening on port ${port}}!\n`);
+server.listen(process.env.PORT || 5000, function() {
+  console.log(`Fangarang listening on port ${port}!\n`);
 });
