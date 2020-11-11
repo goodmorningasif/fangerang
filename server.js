a0f35740421ebf228b9b6ff052be1b82322bd8ba
goodmorningasif <aa@hellofriends.org>
2020-11-10 20:41:37 -0800

    update listener message

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const port = process.env.PORT || 5000;
 express()
   .use(express.static(__dirname + '/dist'))
   .get('/', (req, res) => res.sendFile(path.join(__dirname, 'dist/index.html')))
-  .listen(port, () => console.log(`Listening on port ${port}`));
+  .listen(port, () => console.log(`Listening on port ${port} : ${process.env.PORT}`));
 
