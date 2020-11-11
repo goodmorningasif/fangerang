eb488b1a774d6b363a66dd573185485395110b28
goodmorningasif <aa@hellofriends.org>
2020-11-10 18:15:58 -0800

    Create express

--- server.js
+++ server.js
@@ -1,17 +1,9 @@
-const webpack = require('webpack');
-const webpackDevServer = require('webpack-dev-server');
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 5000;
 
-const config = require('./webpack.common.js');
-const options = {
-  contentBase: '/.dist',
-  hot: true,
-  // host: 'localhost',
-}
-webpackDevServer.addDevServerEntrypoints(config, options);
-const compiler = webpack(config);
-const server = new webpackDevServer(compiler, options);
-const port = process.env.PORT || 80;
+express()
+  .use(express.static(__dirname + '/dist'))
+  .get('/', (req, res) => res.sendFile(path.join(__dirname, 'dist/index.html')))
+  .listen(port, console.log(`Listening on port ${port}`));
 
-server.listen(port, function() {
-  console.log(`Fangarang listening on port ${port}!\n`);
-});
