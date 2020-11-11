e2eca251e88b87507a6e891f08d0125fd6d5674e
goodmorningasif <aa@hellofriends.org>
2020-11-10 20:23:40 -0800

    revert webpack production

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,12 +3,5 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
-  devtool: 'source-map',
-  devServer: {
-    compress: true,
-    inline: true,
-    allowedHosts: [
-      '.herokuapp.com'
-  ]
-  }
+  devtool: 'source-map'
 })
