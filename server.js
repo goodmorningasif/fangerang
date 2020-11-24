7ffeee6782cd2f68761f538295f3deccd78c8088
goodmorningasif <aa@hellofriends.org>
2020-11-23 20:59:28 -0800

    update server to point to jsx

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 5000;
 
-express()
-  .use(express.static(__dirname + '/dist'))
-  // .get('/', (req, res) => res.sendFile(path.join(__dirname, 'dist/index.html')))
-  .get('/', (res, req) => res.send(JSON.stringify({ Hello: 'World'})))
-  .listen(port, () => console.log(`Listening on port ${port} : ${process.env.PORT}`));
+const app = express();
+
+  
+app.set('view engine', 'jsx');
+
+app.use(express.static(__dirname + '/dist'));
+
+// .get('/', (req, res) => res.sendFile(path.join(__dirname, 'dist/index.html')))
+app.get('/', (res, req) => res.send(JSON.stringify({ Hello: 'World'})));
+
+app.listen(port, () => console.log(`Listening on port ${port} : ${process.env.PORT}`));
 
