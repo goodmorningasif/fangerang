bbf328a71fa3fed898aedbca9bd90182d737c0a1
goodmorningasif <aa@hellofriends.org>
2020-12-03 21:04:20 -0800

    create static express page

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const path = require('path');
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 300;
 
 const app = express();
 
   
-app.set('view engine', 'jsx');
+// app.set('view engine', 'jsx');
 
-app.use(express.static(__dirname + '/dist'));
+// app.use(express.static(__dirname + '/dist'));
 
 // .get('/', (req, res) => res.sendFile(path.join(__dirname, 'dist/index.html')))
-app.get('/', (res, req) => res.send(JSON.stringify({ Hello: 'World'})));
+// app.get('/', (res, req) => res.send(JSON.stringify({ Hello: 'World'})));
+
+app.use(express.static("public"))
+
+app.get("/", function (req, res) {
+  res.send("<h1>Hello World!</h1>")
+})
 
 app.listen(port, () => console.log(`Listening on port ${port} : ${process.env.PORT}`));
 
