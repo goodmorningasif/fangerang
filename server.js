228e9219ff3e003cf490f8160a6cc6e4132d6e27
goodmorningasif <aa@hellofriends.org>
2020-12-03 20:09:16 -0800

    add env file

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const port = process.env.PORT || 5000;
 const app = express();
 
   
-// app.set('view engine', 'jsx');
+app.set('view engine', 'jsx');
 
-// app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist'));
 
 // .get('/', (req, res) => res.sendFile(path.join(__dirname, 'dist/index.html')))
-// app.get('/', (res, req) => res.send(JSON.stringify({ Hello: 'World'})));
+app.get('/', (res, req) => res.send(JSON.stringify({ Hello: 'World'})));
 
 app.listen(port, () => console.log(`Listening on port ${port} : ${process.env.PORT}`));
 
