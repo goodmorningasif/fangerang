d36266cb66f4fc7932a7521881f773fa1af5fc37
goodmorningasif <aa@hellofriends.org>
2020-12-07 19:27:46 -0800

    fix package scripts

--- server.js
+++ server.js
@@ -13,8 +13,10 @@ const app = express();
 // app.get('/', (res, req) => res.send(JSON.stringify({ Hello: 'World'})));
 
 app.use(express.static("public"))
-  .get("/", function (req, res) {
-    res.send("<h1>Hello World!</h1>")
-  })
-  .listen(port, () => console.log(`Listening on port ${port} : ${process.env.PORT}`));
+
+app.get("/", function (req, res) {
+  res.send("<h1>Hello World!</h1>")
+})
+
+app.listen(port, () => console.log(`Listening on port ${port} : ${process.env.PORT}`));
 
