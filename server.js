42debccb5b5cd87777af2a1af71b9854940aab20
goodmorningasif <aa@hellofriends.org>
2020-12-07 19:45:39 -0800

    set server to app

--- server.js
+++ server.js
@@ -2,21 +2,12 @@ const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 3000;
 
-const app = express();
-
-  
-// app.set('view engine', 'jsx');
-
-// app.use(express.static(__dirname + '/dist'));
-
-// .get('/', (req, res) => res.sendFile(path.join(__dirname, 'dist/index.html')))
-// app.get('/', (res, req) => res.send(JSON.stringify({ Hello: 'World'})));
-
-app.use(express.static("public"))
-
-app.get("/", function (req, res) {
-  res.send("<h1>Hello World!</h1>")
-})
-
-app.listen(port, () => console.log(`Listening on port ${port} : ${process.env.PORT}`));
-
+express()
+  .set('view engine', 'jsx')
+  .use(express.static(__dirname + '/dist'))
+  .get('/', 
+    (req, res) => res.sendFile(path.join(__dirname, 'dist/index.html'))
+  )
+  .listen(port, 
+    () => console.log(`Listening on port ${port}`)
+  )
